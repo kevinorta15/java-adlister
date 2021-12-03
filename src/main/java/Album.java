ee029e5f2ab2c9d9bfdce74f9e7b035141534e51
Kevin Orta <orta.kevin@gmail.com>
2021-12-03 11:11:55 -0600

    javabeans cooked

---
+++ src/main/java/Album.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class Album implements Serializable {
+    private int sales;
+    private String artistName;
+    private String albumName;
+    private int releaseDate;
+    private String genre;
+
+    public Album(){}
+}
---
+++ src/main/java/Quotes.java
@@ -0,0 +1,7 @@
+import java.io.Serializable;
+
+public class Quotes implements Serializable {
+    private String firstName;
+    private String lastName;
+    private int id;
+}
